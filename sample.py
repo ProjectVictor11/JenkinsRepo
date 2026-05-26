e63415b4c70449a189b9d13c895afa68ae58a6a3
Amit Kumar <48339890+ProjectVictor11@users.noreply.github.com>
2026-05-26 19:23:42 +0530

    committing error
    Fix typo in print statement for error message.

--- sample.py
+++ sample.py
@@ -4,3 +4,5 @@ print("Hello! This code is taken from GIT repo")
 print("Edit 1 - verifying VCS file updates")
 print("Edit 2 - verifying Git pool trigger")
 print("Edit 3 - verifying Email configurations of Jenkins")
+prnt("ERROR")
+
