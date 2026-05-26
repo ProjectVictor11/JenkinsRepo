28e03be8d99c777935afd1d2bafbeeea4b851b78
Amit Kumar <48339890+ProjectVictor11@users.noreply.github.com>
2026-05-26 18:30:01 +0530

    Fix syntax error in print statement

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 # Sample python code to be run by Jenkins Trigger
 
 print("Hello! This code is taken from GIT repo")
-print("Edit 1 - verifying VCS file updates)
+print("Edit 1 - verifying VCS file updates")
 print("Edit 2 - verifying Git pool trigger")
