5728b86be96ecf4ef2449974681746673748df35
Amit Kumar <48339890+ProjectVictor11@users.noreply.github.com>
2026-05-26 19:14:59 +0530

    Add print statement for Jenkins email configuration

--- sample.py
+++ sample.py
@@ -3,3 +3,4 @@
 print("Hello! This code is taken from GIT repo")
 print("Edit 1 - verifying VCS file updates")
 print("Edit 2 - verifying Git pool trigger")
+print("Edit 3 - verifying Email configurations of Jenkins")
